99b9f1785ec75885c39ab36fb368b067721906de
JacksonSmithStudent <jasm076@student.oaisd.org>
2025-02-04 09:17:49 -0500

    added quite a bit

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -1,23 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Trigger : MonoBehaviour
 {
-
+    [SerializeField] bool hideOnTriggerEnter;
+    [SerializeField] string tagFilter;
     [SerializeField] UnityEvent onTriggerEnter;
     [SerializeField] UnityEvent onTriggerExit;
 
-        void OnTriggerEnter(Collider other)
+    private bool hasTriggered = false; // Prevents multiple activations
+
+    void OnTriggerEnter(Collider other)
     {
+        if (hasTriggered) return; // If already triggered, exit immediately
+        if (!string.IsNullOrEmpty(tagFilter) && !other.gameObject.CompareTag(tagFilter)) return;
+
+        hasTriggered = true; // Mark as triggered
         onTriggerEnter.Invoke();
+
+        if (hideOnTriggerEnter && gameObject.name == "Mesh_protective suit")
+        {
+            Invoke(nameof(HideObject), 2f); // Hides the object after 2 seconds
+        }
     }
+
     void OnTriggerExit(Collider other)
     {
+        if (!string.IsNullOrEmpty(tagFilter) && !other.gameObject.CompareTag(tagFilter)) return;
         onTriggerExit.Invoke();
     }
 
-
+    void HideObject()
+    {
+        gameObject.SetActive(false); // Fully hides the GameObject
+    }
 }
-
